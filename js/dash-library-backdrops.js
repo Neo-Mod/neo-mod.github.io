4f9a4295ee7f18f9c92a41984d60d16ebcbd0979
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-07-18 05:02:42 -0400

    a

---
+++ js/dash-library-backdrops.js
@@ -0,0 +1,15 @@
+(window["webpackJsonpGUI"] = window["webpackJsonpGUI"] || []).push([["dash-library-backdrops"],{
+
+/***/ "./src/lib/libraries/dash-assets/generated-backdrops.json":
+/*!****************************************************************!*\
+  !*** ./src/lib/libraries/dash-assets/generated-backdrops.json ***!
+  \****************************************************************/
+/*! exports provided: 0, 1, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("[{\"name\":\"Confetti\",\"tags\":[\"dash\",\"patterns\"],\"bitmapResolution\":1,\"dataFormat\":\"svg\",\"rotationCenterX\":244,\"rotationCenterY\":187.5,\"src\":{\"library\":\"dash\",\"path\":\"/backdrops/Confetti.svg\"}},{\"name\":\"Gaming Shapes\",\"tags\":[\"dash\",\"patterns\"],\"bitmapResolution\":1,\"dataFormat\":\"svg\",\"rotationCenterX\":240,\"rotationCenterY\":180.5,\"src\":{\"library\":\"dash\",\"path\":\"/backdrops/Gaming%20Shapes.svg\"}}]");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=dash-library-backdrops.js.map
---
+++ js/dash-library-sounds.js
@@ -0,0 +1,15 @@
+(window["webpackJsonpGUI"] = window["webpackJsonpGUI"] || []).push([["dash-library-sounds"],{
+
+/***/ "./src/lib/libraries/dash-assets/generated-sounds.json":
+/*!*************************************************************!*\
+  !*** ./src/lib/libraries/dash-assets/generated-sounds.json ***!
+  \*************************************************************/
+/*! exports provided: 0, 1, 2, 3, 4, 5, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("[{\"name\":\"Click\",\"tags\":[\"dash\",\"effects\"],\"dataFormat\":\"\",\"sampleCount\":8064,\"rate\":44100,\"src\":{\"library\":\"dash\",\"path\":\"/sounds/Click.mp3\"}},{\"name\":\"Coins\",\"tags\":[\"dash\",\"effects\",\"games\"],\"dataFormat\":\"\",\"sampleCount\":73728,\"rate\":48000,\"src\":{\"library\":\"dash\",\"path\":\"/sounds/Coins.mp3\"}},{\"name\":\"Gems\",\"tags\":[\"dash\"],\"dataFormat\":\"\",\"sampleCount\":69120,\"rate\":48000,\"src\":{\"library\":\"dash\",\"path\":\"/sounds/Gems.mp3\"}},{\"name\":\"Repair\",\"tags\":[\"dash\",\"effects\"],\"dataFormat\":\"\",\"sampleCount\":147456,\"rate\":44100,\"src\":{\"library\":\"dash\",\"path\":\"/sounds/Repair.mp3\"}},{\"name\":\"Sword\",\"tags\":[\"dash\",\"effects\"],\"dataFormat\":\"\",\"sampleCount\":64512,\"rate\":48000,\"src\":{\"library\":\"dash\",\"path\":\"/sounds/Sword.mp3\"}},{\"name\":\"Wow\",\"tags\":[\"dash\",\"effects\",\"voice\",\"cartoon\"],\"dataFormat\":\"\",\"sampleCount\":185472,\"rate\":44100,\"src\":{\"library\":\"dash\",\"path\":\"/sounds/Wow.mp3\"}}]");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=dash-library-sounds.js.map
